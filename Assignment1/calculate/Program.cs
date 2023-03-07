a9daecbf1d952ed73bb2f4f33e34ad3540f44560
Ella-zsy <1564748059@qq.com>
2023-03-07 19:37:59 +0800

    homework1
    homework1

---
+++ Assignment1/calculate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculate
+{
+    internal class Program
+    {
+        static void calculator(int a, string p, int b)
+        {
+            if (p == "+")
+                Console.WriteLine($"a+b={a + b}");
+            if (p == "-")
+                Console.WriteLine($"a-b={a - b}");
+            if (p == "*")
+                Console.WriteLine($"a*b={a * b}");
+            if (p == "/")
+                Console.WriteLine($"a/b={a / b}");
+        }
+        static void Main(string[] args)
+        {
+            string s = "";
+            string p = "";
+            int a = 0;
+            int b = 0;
+            Console.Write("Please input an number:");
+            s = Console.ReadLine();
+            a = int.Parse(s);
+
+            Console.Write("Please input an operator:");
+            p = Console.ReadLine();
+
+            Console.Write("Please input an number:");
+            s = Console.ReadLine();
+            b = int.Parse(s);
+
+            calculator(a, p, b);
+
+        }
+    }
+}
---
+++ Assignment1/calculators/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace calculators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c = 0;
+            switch (textBox4.Text)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+
+            }
+            textBox3.Text = c.ToString();
+        }
+
+        
+    }
+}
