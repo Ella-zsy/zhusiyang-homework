1fce58a2c7e77e72edae45aabca5eea7f0f677aa
Ella-zsy <1564748059@qq.com>
2023-05-08 17:20:34 +0800

    homework 9
    homework 9

---
+++ Assignment9/Assignment9/Controllers/OrderController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Assignment9.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Assignment9.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController: ControllerBase
+    {
+        private readonly OrderService orderService;
+
+        public OrderController(OrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+        // GET: api/Order
+        [HttpGet]
+        public ActionResult<List<Order>> GetOrders()
+        {
+            return orderService.GetAllOrders();
+        }
+
+        // GET: api/Order/1
+        [HttpGet("{id}")]
+
+        //对订单的查找
+        public ActionResult<Order> GetOrder(string id)
+        {
+            var order = orderService.GetOrder(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return order;
+        }
+
+        // POST: api/Order
+        [HttpPost]
+
+        //对订单的添加
+        public ActionResult<Order> AddOrder(Order order)
+        {
+            try
+            {
+                order.OrderId = Guid.NewGuid().ToString();
+                orderService.AddOrder(order);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+
+            return order;
+        }
+
+        // PUT: api/Order/1
+        [HttpPut("{id}")]
+
+        //对订单的更改
+        public ActionResult<Order> updateOrder(string id, Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                orderService.UpdateOrder(order);
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                if (e.InnerException != null) error = e.InnerException.Message;
+                return BadRequest(error);
+            }
+            return NoContent();
+        }
+
+        // DELETE: api/Order/1
+        [HttpDelete("{id}")]
+
+        //对订单的删除
+        public ActionResult<Order> DeleteOrder(string id)
+        {
+            try
+            {
+                orderService.RemoveOrder(id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+            return NoContent();
+        }
+
+    }
+}
---
+++ Assignment9/Assignment9/models/OrderContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Assignment9.models
+{
+    public class OrderContext: DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options)
+         : base(options)
+        {
+            this.Database.EnsureCreated(); //自动建库建表
+        }
+        //定义有哪几种模式
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Goods> Goods { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
